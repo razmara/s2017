0819c9d837bfb29e706affa2cfc0a00572770f90
ttoocs <ttoocs@gmail.com>
2017-05-03 13:33:13 -0600

    changed formats, caused bugs, bugs fixed.

--- dataset/get_data.sh
+++ dataset/get_data.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-#As not all seqences have corrisponding models, download the model first, then get it's seqence.
+WGET_ARGS="--continue "
+ADIR=`pwd`
+DLDIR=$ADIR/dl
+
+
+mkdir -p $DLDIR
+
 for i in {00000..99999}; do
-  wget "http://3dscan.stanford.edu/data/mesh/"$i".ply" &&\
-  wget "http://3dscan.stanford.edu/data/seq/"$i".zip"
+  cd $DLDIR
+
+  #As not all seqences have corrisponding models, download the model first, then get it's seqence.
+  wget "http://3dscan.stanford.edu/data/mesh/"$i".ply" $WGET_ARGS &&\
+  wget "http://3dscan.stanford.edu/data/seq/"$i".zip" $WGET_ARGS || \
+  continue
+
+#Comment this out to prevent automatic extraction.
+  cd $ADIR
+
+  mkdir $i
+  cd $i
+  unzip $DLDIR/$i.zip
+  ln -s $DLDIR/$i.ply ./
+  cd $ADIR
+
 done
 
