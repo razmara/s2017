32af1d2cda40e04ccb2a4e07aa0b8cd5cf880261
ttoocs <ttoocs@gmail.com>
2017-08-03 15:01:09 +0000

    Path setup script (use source on it to get access to all the misc programs from this git)

---
+++ setup_path.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/pipeline.sh
+PATH=$DIR/pcl/build/bin:$PATH
+PATH=$DIR/3d-re/bin:$PATH
+PATH=$DIR/oni_stuff/ni2Recorder/build:$PATH
+PATH=$DIR/oni_stuff/ni1Recorder/build:$PATH
+PATH=$DIR/oni_stuff/simpleReader1/build:$PATH
+PATH=$DIR/oni_stuff/simpleReader2/build:$PATH
+PATH=$DIR/utils/build/bin:$PATH
+
+
