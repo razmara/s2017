fb4e5e42dd54e2affa8cda58f69a30ab862046b6
ttoocs <ttoocs@test.metaoptima>
2017-08-04 11:52:58 +0000

    Apacman for flann

--- setup/setup_dirs.sh
+++ setup/setup_dirs.sh
@@ -7,25 +7,35 @@ updateSub(){
   git submodule update --remote ./
 }
 
+echo "Apacman"
+sudo pacman -S --needed --asdeps jshon
+cd /tmp
+curl -O "https://raw.githubusercontent.com/oshazard/apacman/master/apacman"
+sudo bash ./apacman -S apacman --noconfirm
+apacman -S apacman-deps --noconfirm
+
 echo "OpenNI2"
 cd $MASTERDIR
 cd OpenNI2
 updateSub
 git checkout devmerge
 cd aur
-makepkg -s -i
+makepkg -s -i -f
 
 echo "LibFreenect2"
 cd $MASTERDIR
 cd libfreenect2
 updateSub
 cd aur
-makepkg -s -i
+makepkg -s -i -f
+
+
 
 echo "PCL"
 cd $MASTERDIR
 cd setup/pcl
-makepkg -s -i
+apacman -S flann --noconfirm
+makepkg -s -i -f
 
 echo "ER"
 cd $MASTERDIR
