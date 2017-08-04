fd79699e44cf6a95ede492d95185cf4f3149ae57
ttoocs <ttoocs@gmail.com>
2017-08-04 11:24:28 +0000

    ccache

--- setup/pacman_installs.sh
+++ setup/pacman_installs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-pacman -S base base-devel git cuda opencv cmake sudo openssh vim htop
+pacman -S base base-devel git cuda opencv cmake sudo openssh vim htop ccache
+
+#Enables CCache for all users. (Helpfull for re-building pcl)
+echo 'export PATH="/usr/lib/ccache/bin/:$PATH"' >> /etc/bash.bashrc
 
