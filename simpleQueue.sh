d48ec7080a64c6764fcce710aa9c523593c79be6
ttoocs <ttoocs@gmail.com>
2017-08-11 12:02:02 -0600

    Due to wanting to run ltos of tests overnight, and most failing due to memory restrictions, Implmented a simple queue to run them.

---
+++ simpleQueue.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#This is to let me run various programs, sequentally, by simply adding them to a queue.
+
+
+QUEUE="/tmp/myque"
+
+#This is the server.
+QServer(){
+  cd /tmp
+
+  qi=0;
+  while true ; do
+    if [ -e $QUEUE ] ; then
+      eval $(head -n 1 $QUEUE) 2>&1 | tee queue_$qi.log
+      tail -n +2 $QUEUE > $QUEUE.tmp
+      mv $QUEUE.tmp $QUEUE
+      let qi++
+    else
+      sleep 5;
+    fi
+   
+    if [ -e $QUEUE.add ]; then 
+      mv $QUEUE.add $QUEUE.tmp
+      cat $QUEUE.tmp >> $QUEUE
+      rm $QUEUE.tmp
+    fi
+  done
+
+}
+
+#Add to queue
+QAdd(){
+  echo " cd \"`pwd`\" ; $@" > $QUEUE.add
+}
+
+#Remove from queue
+#QRem(){
+#  
+#}
