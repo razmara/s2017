9e13acec6035503917b4b9cc0a0d58e6cd808a27
ttoocs <ttoocs@gmail.com>
2017-08-25 14:08:30 -0600

    Simple submodule setter-upper-getter.

---
+++ setup/submodules.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MASTERDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
+
+updateSub(){
+  cd $MASTERDIR
+  cd $1
+  git submodule init ./
+  git submodule update --remote ./
+}
+
+updateSub ElasticReconstructionRelease
+updateSub er
+updateSub libfreenect2
+updateSub pcl
+updateSub spcl
+updateSub openCV_Traj
+updateSub OpenNI2
+updateSub SeperateKinfu
