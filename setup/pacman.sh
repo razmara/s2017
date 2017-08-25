9fb53b87b111a9f238fd2ad7f4592d772dd79733
ttoocs <ttoocs@gmail.com>
2017-08-25 14:34:57 -0600

    Added two packages which seem to be needed by vtk, which is needed by ElasticReconstruction.

--- setup/pacman.sh
+++ setup/pacman.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -Syu --needed base base-devel git cuda opencv cmake sudo openssh vim htop ccache jshon qt5
+pacman -Syu --needed base base-devel git cuda opencv cmake sudo openssh vim htop ccache jshon qt5 unixodbc gdal
 
 #Enables CCache for all users. (Helpfull for re-building pcl)
 echo 'export PATH="/usr/lib/ccache/bin/:$PATH"' >> /etc/bash.bashrc
