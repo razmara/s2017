472fd60640571f4790ee38eba4c450159638d7e0
ttoocs <ttoocs@gmail.com>
2017-08-30 19:11:25 +0000

    Removed ccache from the pacman script.

--- setup/pacman.sh
+++ setup/pacman.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -Syu --needed base base-devel git cuda opencv cmake sudo openssh vim htop ccache jshon qt5 unixodbc gdal jdk8-openjdk nvidia
+pacman -Syu --needed base base-devel git cuda opencv cmake sudo openssh vim htop jshon qt5 unixodbc gdal jdk8-openjdk nvidia
 
 #Enables CCache for all users. (Helpfull for re-building pcl)
 echo 'export PATH="/usr/lib/ccache/bin/:$PATH"' >> /etc/bash.bashrc
